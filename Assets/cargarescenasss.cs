9f0cc644bf86be567fe387fc7ad1a2ee493d43d8
licalexl <60006370+licalexl@users.noreply.github.com>
2024-06-18 22:38:54 -0600

    CAMBIOS FINALES

--- Assets/cargarescenasss.cs
+++ Assets/cargarescenasss.cs
@@ -5,25 +5,80 @@ using UnityEngine.SceneManagement;
 
 public class cargarescenasss : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject pausaCanvas;
+    public GameObject creditosCanvas;
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.P))
+        {
+            if (Time.timeScale == 1)
+            {
+                PausarJuego();
+            }
+            else
+            {
+                ReanudarJuego();
+            }
+        }
+    }
     void Start()
     {
-        Cursor.lockState = CursorLockMode.None;
+        //Cursor.lockState = CursorLockMode.None;
+        //Cursor.visible = true;
+    }
+
+    public void PausarJuego()
+    {
+        Time.timeScale = 0;
+        pausaCanvas.SetActive(true);
         Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
+
+    public void ReanudarJuego()
+    {
+        Time.timeScale = 1;
+        pausaCanvas.SetActive(false);
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     public void RecargarEscena()
     {
-
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         Time.timeScale = 1;
-
-
     }
 
-    public void CambiarDeEscena()
+    public void CambiarDeEscenaLevel1()
     {
         SceneManager.LoadScene("Level1");
         Time.timeScale = 1;
     }
+
+    public void CambiarMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+        Time.timeScale = 1;
+    }
+
+    public void abrirCreditos()
+    {
+        creditosCanvas.gameObject.SetActive(true);      
+    }
+
+    public void cerrarCreditos()
+    {
+        creditosCanvas.gameObject.SetActive(false);
+    }
+
+    public void Salir()
+    {
+        #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+        #else       
+        Application.Quit();
+        #endif
+    }
+
+
 }
